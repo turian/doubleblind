afa387dff1bf998ccd7836a76a5d5899f37b9c4a
Joseph Turian <turian@gmail.com>
2009-07-25 19:24:35 -0400

    Can now get anonymous twitter feed

--- feed/views.py
+++ feed/views.py
@@ -1,7 +1,25 @@
-# Create your views here.
-from doubleblind.feed.models import Post, Rating
 from django.http import HttpResponse
+from django.shortcuts import render_to_response
+from django.template import RequestContext
 
-def twitter_feed(request):
-	return HttpResponse("feed") 
 
+import simplejson
+
+import twitter
+twitterapi = twitter.Api()      # NOTE: This is an unauthenticated twitter
+                                # instance, we should use the OAuth information to authenticate
+
+import re
+usernamere = re.compile("@\S+")
+
+def twitter_feed(request, user):
+    """
+    TODO: Want to get the user's timeline, not the public timeline.
+    """
+    #timeline = twitterapi.GetFriendsTimeline(user, count=200)
+    #timeline = twitterapi.GetFriendsTimeline(user)
+    timeline = twitterapi.GetPublicTimeline()
+    for status in timeline:
+        status.blind_text = status.GetText()
+        status.blind_text = usernamere.sub("@anonymous", status.blind_text)
+    return render_to_response("feed.html", {"timeline": timeline}, context_instance=RequestContext(request))
