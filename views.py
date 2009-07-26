2d562bb71746dbb9898425d30d2de5a2f8392ad0
Joseph Turian <turian@gmail.com>
2009-07-26 04:38:07 -0400

    Check and handle if friendfeed authentication is wrong

--- views.py
+++ views.py
@@ -2,9 +2,18 @@ from django.http import HttpResponse,HttpResponseRedirect
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 
+from urllib2 import HTTPError
+
+from feed.views import friendfeed_initialize
+from ffauth.views import logout
+
 def start(request):
     # TODO: Better way of seeing if user is authenticated
     if 'username' in request.session:
+        try:
+            friendfeed_initialize(request)
+        except HTTPError:            
+            return logout(request, msg="There was a problem authenticating you. Please try again.")
         return render_to_response("start.html", {"username": request.session["username"]}, context_instance=RequestContext(request))
     else:
         return HttpResponseRedirect("/login")
