422c1aa08e9e0b52d95ef2f4ad63f67a1485a4c0
ricree <ricree@gmail.com>
2009-07-25 20:37:09 -0500

    added views and models for auth

---
+++ ffauth/views.py
@@ -0,0 +1,19 @@
+# Create your views here.
+from django.http import HttpResponse,HttpResponseRedirect
+from django.shortcuts import render_to_response
+from doubleblind.ffauth.forms import FFLoginForm
+def login(request):
+	if request.method=='POST':
+		#user is authenticated if form validates
+		form = FFLoginForm(request.POST)
+		if(form.is_valid()):
+			#login logic here
+			#fields accessed via form.cleaned_data[fieldname]
+			request.session['username'] = form.cleaned_data['username']
+			request.session['remote_key'] = form.cleaned_data['remote_key']
+			HttpResponseRedirect("/feed/")
+		else:
+	else:
+		form = FFLoginForm()
+	return render_to_response("fflogin.html",{'form':form})
+	
